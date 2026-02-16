6488c9cc5233a92edac677021122406e7ae8fecf
dq2024 <dq2024@nyu.edu>
2026-02-16 12:12:05 -0500

    colbert test

--- indexing.py
+++ indexing.py
@@ -152,10 +152,10 @@ if __name__ == '__main__':
     if args.do_training:
         print(f'Starting ColBERTv2 Training on QAMPARI')
         
-        with Run().context(RunConfig(nranks=4, experiment='qampari_colbertv2.0_lr1e-5')):
+        with Run().context(RunConfig(nranks=4, experiment='qampari_colbertv2.0_lr5e-6')):
             config = ColBERTConfig(
                 bsize=32,                       
-                lr=1e-05,                      
+                lr=5e-6,                      
                 warmup=20_000,                 
                 doc_maxlen=doc_maxlen,                
                 dim=128,                       
